5de1a847698f5ffd1ae305946b13fe53620f9c0b
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-08-14 20:36:24 +0100

    Make it safe to call loadApi more than once. On subsequent calls, the onLoad callback is called immediately.

--- src/kong/Kongregate.hx
+++ src/kong/Kongregate.hx
@@ -1,6 +1,11 @@
 package kong;
 
 class Kongregate {
+    #if flash8
+    #elseif flash
+    static var api:KongregateApi = null;
+    #end
+
     /** Retrieves the Kongregate API object, and calls [onLoad(api)] when complete.
      *
      * After retrieving the Kongregate API object you should call [api.services.connect()] to
@@ -12,20 +17,24 @@ class Kongregate {
     }
     #elseif flash
     public static function loadApi(onLoad:KongregateApi->Void) {
-        var parameters = flash.Lib.current.root.loaderInfo.parameters;
-        var apiPath = parameters.kongregate_api_path;
-        if (apiPath == null) {
-            apiPath = "http://www.kongregate.com/flash/API_AS3_Local.swf";
+        if (api == null) {
+            var parameters = flash.Lib.current.root.loaderInfo.parameters;
+            var apiPath = parameters.kongregate_api_path;
+            if (apiPath == null) {
+                apiPath = "http://www.kongregate.com/flash/API_AS3_Local.swf";
+            }
+            flash.system.Security.allowDomain(apiPath);
+            var request = new flash.net.URLRequest(apiPath);
+            var loader = new flash.display.Loader();
+            loader.contentLoaderInfo.addEventListener(flash.events.Event.COMPLETE, function(event:flash.events.Event) {
+                api = cast event.target.content;
+                onLoad(api);
+            });
+            loader.load(request);
+            flash.Lib.current.addChild(loader);
+        } else {
+            onLoad(api);
         }
-        flash.system.Security.allowDomain(apiPath);
-        var request = new flash.net.URLRequest(apiPath);
-        var loader = new flash.display.Loader();
-        loader.contentLoaderInfo.addEventListener(flash.events.Event.COMPLETE, function(event:flash.events.Event) {
-            var api = event.target.content;
-            onLoad(cast api);
-        });
-        loader.load(request);
-        flash.Lib.current.addChild(loader);
     }
     #end
 }
